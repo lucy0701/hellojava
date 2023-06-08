48edf24a17b739279de6128696a42afe4058ed52
lucy0701 <sasain71@naver.com>
2023-06-08 11:38:50 +0900

    0608

--- src/ch07/sec05/exam02/SportsCar.java
+++ src/ch07/sec05/exam02/SportsCar.java
@@ -1,5 +1,14 @@
 package ch07.sec05.exam02;
 
-public class SportsCar {
-
+public class SportsCar extends Car {
+	@Override
+	public void speedUp() {
+		speed += 10;
+	}
+	// 오버 라이딩을 할 수 없음.
+	@Override
+	public void stop() {
+		System.out.println("스포츠카를 멈춤");
+		speed = 0;
+	}
 }
