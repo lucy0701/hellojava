4ca21110e8d7665186a617634581e5ccb4323e3e
lucy0701 <sasain71@naver.com>
2023-06-14 09:13:46 +0900

    0618

--- src/crystal/DBConnectionTest.java
+++ src/crystal/DBConnectionTest.java
@@ -1,20 +1,50 @@
 package crystal;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
 
 
 public class DBConnectionTest {
-	String url = "jdbc:mysql://localhost:33061/pangmin?serverTimezone=Asia/Seoul\n"
-			+ "\n"
-			+ "";
-	String ID = "lucy";
-	String PW = "qorhvkd1514";
+	
+//	public static void main(String[] args) {
+//        try {
+//            // MySQL 연결 정보 설정
+//            String url = "jdbc:mysql://localhost:3306/";
+//            String username = "lucy";
+//            String password = "Qorhvkd1514@";
+//
+//            // JDBC 드라이버 로드
+//            Class.forName("com.mysql.cj.jdbc.Driver");
+//
+//            // 연결 생성
+//            Connection connection = DriverManager.getConnection(url, username, password);
+//
+//            // 쿼리 실행
+//            Statement statement = connection.createStatement();
+//            ResultSet resultSet = statement.executeQuery("SELECT * FROM table_name");
+//
+//            // 결과 출력
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString("column_name"));
+//            }
+//
+////            // 연결 종료
+//            resultSet.close();
+//            statement.close();
+//            connection.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 	
 	DBConnectionTest() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("로딩 성공");
-			Connection conn = DriverManager.getConnection(url, ID, PW);
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306", "lucy", "Qorhvkd1514@");
 			System.out.println("서버 연결 성공" + conn);
 		} catch (ClassNotFoundException e) {
 			System.out.println("로딩 실패");
@@ -26,7 +56,8 @@ public class DBConnectionTest {
 	
 	public static void main(String[] args) {
 		new DBConnectionTest();
-	}
-			
+	}			
+
+
 
 }
